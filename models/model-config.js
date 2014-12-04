aa3661a0bac0f997f78c95619317366b31ccca9b
danieljoppi <daniel.joppi@gmail.com>
2014-12-04 13:50:17 -0200

    change models

--- models/model-config.js
+++ models/model-config.js
@@ -6,8 +6,13 @@ module.exports = function(mongoose) {
     var Schema = mongoose.Schema;
 
     // list models
-    require('./company-model.js')(mongoose, Schema);
-    require('./image-model.js')(mongoose, Schema);
-    require('./location-model.js')(mongoose, Schema);
-    require('./user-model.js')(mongoose, Schema);
+//    require('./company-model.js')(mongoose, Schema);
+//    require('./image-model.js')(mongoose, Schema);
+//    require('./location-model.js')(mongoose, Schema);
+//    require('./user-model.js')(mongoose, Schema);
+
+    mongoose.model('company', new Schema({}), { strict: false });
+    mongoose.model('image', new Schema({}), { strict: false });
+    mongoose.model('location', new Schema({}), { strict: false });
+    mongoose.model('user', new Schema({}), { strict: false });
 };
